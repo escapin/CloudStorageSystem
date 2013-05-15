b7316a0805e58d765d540161fa8097a26f8c64ac
Enrico Scapin <scapin@uni-trier.de>
2013-05-15 23:40:20 +0200

    Client and Server created

---
+++ src/de/uni/trier/infsec/clientServer/Client.java
@@ -0,0 +1,11 @@
+package de.uni.trier.infsec.clientServer;
+
+public class Client {
+	
+	public void store(byte[] message){
+		
+	}
+	public byte[] retreive(int label){
+		
+	}
+}
---
+++ src/de/uni/trier/infsec/clientServer/Server.java
@@ -0,0 +1,5 @@
+package de.uni.trier.infsec.clientServer;
+
+public class Server {
+	
+}
