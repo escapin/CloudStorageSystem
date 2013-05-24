289cb293ab2e17fc5aa0fbd41b659f40624a090c
Tomasz Truderung <ttruderung@gmail.com>
2013-05-24 12:33:41 +0200

    refactoring (move LabelList into the Client class)

--- src/de/uni/trier/infsec/cloudStorage/Client.java
+++ src/de/uni/trier/infsec/cloudStorage/Client.java
@@ -5,7 +5,6 @@ import java.util.*;
 import de.uni.trier.infsec.functionalities.pki.idealcor.PKISig;
 import de.uni.trier.infsec.functionalities.pki.idealcor.PKIEnc;
 import de.uni.trier.infsec.functionalities.pki.idealcor.PKIError;
-import de.uni.trier.infsec.functionalities.pki.idealcor.PKIEnc.Encryptor;
 import de.uni.trier.infsec.functionalities.symenc.SymEnc;
 import de.uni.trier.infsec.environment.network.NetworkClient;
 import de.uni.trier.infsec.environment.network.NetworkError;
@@ -179,5 +178,45 @@ public class Client {
 	
 	@SuppressWarnings("serial")
 	public class IncorrectReply extends Exception {}
+	
+	/**
+	 * List of labels.
+	 * For each 'label' maintains an counter representing 
+	 * how many times the label has been used.
+	 */
+	static private class LabelList {
+		
+		static class Node {
+			byte[] key;
+			int counter;
+			Node next;
+			public Node(byte[] key, int counter, Node next) {
+				this.key = key;
+				this.counter = counter;
+				this.next = next;
+			}
+		}
+		
+		private Node firstElement = null;
+		
+		public void put(byte[] key, int counter) {
+			for(Node tmp = firstElement; tmp != null; tmp=tmp.next)
+	            if( Arrays.equals(key, tmp.key) ){
+	                tmp.counter=counter;
+	                return;
+	            }
+			firstElement = new Node(key, counter, firstElement);
+		}
 
+	    public int get(byte[] key) {
+	        for(Node tmp = firstElement; tmp != null; tmp=tmp.next)
+	            if( Arrays.equals(key, tmp.key)  )
+	                return tmp.counter;	
+	        return -1; // if the label is not present, return -1
+	    }
+	    
+	    public boolean containsKey(byte[] key) {
+	    	return get(key) >= 0;
+	    }
+	}
 }
