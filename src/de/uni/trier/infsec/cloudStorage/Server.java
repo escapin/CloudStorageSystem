2db63c2d0c7ac1162b2ecdcb30e741519989f0b4
Enrico Scapin <scapin@uni-trier.de>
2013-05-29 14:10:32 +0200

    userID added to the storage, exception specialized in Client/Server

--- src/de/uni/trier/infsec/cloudStorage/Server.java
+++ src/de/uni/trier/infsec/cloudStorage/Server.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 import de.uni.trier.infsec.environment.network.NetworkError;
 import de.uni.trier.infsec.functionalities.pki.idealcor.*;
-import de.uni.trier.infsec.cloudStorageException.*;
 import de.uni.trier.infsec.utils.MessageTools;
 
 public class Server{
@@ -17,7 +16,7 @@ public class Server{
 	/**
 	 * Process every request coming from a client and reply with the proper response 
 	 */
-	public static byte[] processRequest(byte[] request) throws StorageError, NetworkError, PKIError {
+	public static byte[] processRequest(byte[] request) throws MalformedMessage, NetworkError, PKIError {
 		/*
 		 * Every request has this shape:
 		 * 		Enc_Server{ (clientID, ([payload], signClient)) }
@@ -31,12 +30,12 @@ public class Server{
 		byte[] id = MessageTools.first(id_payload_signClient);
 		if(id.length!=4) // since clientID is supposed to be a integer, its length must be 4 bytes
 			throw new MalformedMessage();
-		int clientID = MessageTools.byteArrayToInt(id);
+		int userID = MessageTools.byteArrayToInt(id);
 		byte[] payload_signClient=MessageTools.second(id_payload_signClient);
 		byte[] payload = MessageTools.first(payload_signClient);
 		byte[] signClient=MessageTools.second(payload_signClient);
 		// verify that the message comes from the client 'clientID'
-		PKISig.Verifier clientVerifier = PKISig.getVerifier(clientID, Params.PKI_DSIG_DOMAIN);
+		PKISig.Verifier clientVerifier = PKISig.getVerifier(userID, Params.PKI_DSIG_DOMAIN);
 		if(!clientVerifier.verify(signClient, payload))
 			throw new MalformedMessage();
 		
@@ -46,9 +45,9 @@ public class Server{
 		if(Arrays.equals(tag, Params.STORE))
 			// we have to store also 'signClient' because when the client asks us to retrieve the message,
 			// he wants to be sure that we are giving him the proper message
-			payloadResp = store(MessageTools.second(payload), signClient);
+			payloadResp = store(userID, signClient, MessageTools.second(payload));
 		else if(Arrays.equals(tag, Params.RETRIEVE))
-			payloadResp =  retrieve(MessageTools.second(payload));
+			payloadResp =  retrieve(userID, MessageTools.second(payload));
 		else
 			throw new MalformedMessage();
 		
@@ -65,17 +64,17 @@ public class Server{
 		byte[] msgSigned = MessageTools.concatenate(signClient_payloadResp, signServer);
 		
 		// encrypt the message for the client and return it
-		PKIEnc.Encryptor clientEncryptor = PKIEnc.getEncryptor(clientID, Params.PKI_ENC_DOMAIN);
+		PKIEnc.Encryptor clientEncryptor = PKIEnc.getEncryptor(userID, Params.PKI_ENC_DOMAIN);
 		
 		return clientEncryptor.encrypt(msgSigned);
 	}
 	
 	/**
 	 * Try to store the message and reply:
-	 * - STORE_OK if the message has been stored correctly
-	 * - (STORE_FAIL, lastCounter) if there already is a message stored with the same label but with an higher counter 
+	 * - STORE_OK:	the message has been stored correctly
+	 * - (STORE_FAIL, lastCounter):	there is already a message stored with the same (userID, label) but with an higher counter 
 	 */
-	private static byte[] store(byte[] label_counter_encMsg, byte[] signClient) throws StorageError{
+	private static byte[] store(int userID, byte[] signClient, byte[] label_counter_encMsg) throws MalformedMessage{
 		// Shape of the message to parse: (label, (counter, encMsg))
 		byte[] label = MessageTools.first(label_counter_encMsg);
 		byte[] counter_encMsg = MessageTools.second(label_counter_encMsg);
@@ -85,35 +84,41 @@ public class Server{
 			throw new MalformedMessage();
 		int counter = MessageTools.byteArrayToInt(counterB);
 		// check whether the counter is higher than the highest counter associated with that 'label'
-		int lastCounter=msgStorage.getLastCounter(label);
+		int lastCounter=msgStorage.getLastCounter(userID, label);
 		if(counter<=lastCounter)
 			// we can't store the message
 			// we have to return the message (STORE_FAIL, lastCounter)
 			return MessageTools.concatenate(Params.STORE_FAIL, MessageTools.intToByteArray(lastCounter));
 		// otherwise we can store the message
-		msgStorage.insert(label, counter, encMsg, signClient);
-		// we have to return STORE_OK
-		return Params.STORE_OK;	
+		msgStorage.insert(userID, label, counter, encMsg, signClient);
+		return Params.STORE_OK;
 	}
 	
 	
 	/**
-	 * Try to retrieve the message indexed with (label, counter) and reply:
-	 * - (RETRIEVE_OK, (encMsg, signEncrMsg)) the message and the signature of it when it was sent to the server 
-	 * - RETREIVE_FAIL	if there isn't any message stored corresponding to (label, counter) pair
+	 * Try to retrieve the message indexed with (userID, label, counter) and reply:
+	 * - (RETRIEVE_OK, (encMsg, signEncrMsg)):	the message and the signature of it when it was sent to the server 
+	 * - RETREIVE_FAIL:	there isn't any message indexed with (userID, label, counter)
 	 */
-	private static byte[] retrieve(byte[] label_counter) throws StorageError{
+	private static byte[] retrieve(int userID, byte[] label_counter) throws MalformedMessage{
 		// Shape of the message to parse: (label, counter)
 		byte[] label = MessageTools.first(label_counter);
 		byte[] counterB = MessageTools.second(label_counter);
 		if(label.length==0 || counterB.length!=4)
 			throw new MalformedMessage();
 		int counter = MessageTools.byteArrayToInt(counterB);
-		byte[] encMsg = msgStorage.getMessage(label, counter);
+		byte[] encMsg = msgStorage.getMessage(userID, label, counter);
 		if(encMsg==null) // the message isn't in the storage
 			return Params.RETRIEVE_FAIL;
-		byte[] signEncMsg = msgStorage.getSignature(label, counter);
+		byte[] signEncMsg = msgStorage.getSignature(userID, label, counter);
 		byte[] msg_sign = MessageTools.concatenate(encMsg, signEncMsg);
 		return MessageTools.concatenate(Params.RETRIEVE_OK, msg_sign);
 	}
+	
+	/**
+	 * Exception thrown when the request we received does not conform
+	 * to an expected format (we get, for instance, a trash message). 
+	 */
+	@SuppressWarnings("serial")
+	public static class MalformedMessage extends Exception {}
 }
--- src/de/uni/trier/infsec/cloudStorage/Storage.java
+++ src/de/uni/trier/infsec/cloudStorage/Storage.java
@@ -11,50 +11,60 @@ import de.uni.trier.infsec.utils.MessageTools;
  *
  */
 public class Storage {
+	
 	private Map<KeyStorage,byte[]> storage;
 	
 	/**
 	 * For each label inserted maintains the higher counter
 	 */
-	private Map<String, Integer> maxCount;
+	private Map<KeyStorage, Integer> maxCount;
 	
 	public Storage(){
 		storage = new HashMap<KeyStorage,byte[]>();
-		maxCount = new HashMap<String, Integer>();
+		maxCount = new HashMap<KeyStorage, Integer>();
 	}
 	
 	/**
-	 * Store a message and its signature under the index (label, counter)
+	 * Store a message and its signature under the index (userID, label, counter)
 	 */
-	public void insert(byte[] label, int counter, byte[] msg, byte[] msgSign){
-		storage.put(new KeyStorage(label.toString(), counter), MessageTools.concatenate(msg, msgSign));
+	public void insert(int userID, byte[] label, int counter, byte[] msg, byte[] msgSign){
+		storage.put(new KeyStorage(userID, label.toString(), counter), MessageTools.concatenate(msg, msgSign));
+		KeyStorage k = new KeyStorage(userID, label.toString(), -1);
 		Integer lastCount;
-		if ( (lastCount = maxCount.get(label.toString())) == null || counter>lastCount.intValue())
-				maxCount.put(label.toString(), new Integer(counter));
+		if ( (lastCount = maxCount.get(k)) == null || counter>lastCount.intValue())
+				maxCount.put(k, new Integer(counter));
 	}
 
 	/**
-	 * Retrieve the message associated to the index (label, index)
+	 * Retrieve the message associated to the index (userID, label, index) if it's in the storage,
+	 * null otherwise
 	 */
-	public byte[] getMessage(byte[] label, int counter){
-		byte[] msg_sign = storage.get(new KeyStorage(label.toString(), counter)); 
-		return MessageTools.first(msg_sign);
+	public byte[] getMessage(int userID, byte[] label, int counter){
+		byte[] msg_sign = storage.get(new KeyStorage(userID, label.toString(), counter));
+		if(msg_sign!=null)
+			return MessageTools.first(msg_sign);
+		else
+			return null;
 	}
 	
 	/**
-	 * Retrieve the signature associated to the index (label, index)
+	 * Retrieve the signature associated to the index (userID, label, index) if if it's in the storage,
+	 * null otherwise
 	 */
-	public byte[] getSignature(byte[] label, int counter){
-		byte[] msg_sign = storage.get(new KeyStorage(label.toString(), counter)); 
-		return MessageTools.second(msg_sign);
+	public byte[] getSignature(int userID, byte[] label, int counter){
+		byte[] msg_sign = storage.get(new KeyStorage(userID, label.toString(), counter)); 
+		if(msg_sign!=null)
+			return MessageTools.second(msg_sign);
+		else
+			return null;
 	}
 	
 	/**
-	 * Return the higher counter associated with a particular label
+	 * Return the higher counter associated with a particular (userID, label)
 	 */
-	public int getLastCounter(byte[] label){
+	public int getLastCounter(int userID, byte[] label){
 		// What if maxCount.get(label.toString()) is null?
-		return maxCount.get(label.toString()).intValue();
+		return maxCount.get(new KeyStorage(userID, label.toString(), -1)).intValue();
 	}
 
 	/**
@@ -62,12 +72,17 @@ public class Storage {
 	 * so that we can use the method "equals" 
 	 */
 	private class KeyStorage{
+		private int userID;
 		private String label;
 		private int count;
 		
-		public KeyStorage(String label, int count){
+		public KeyStorage(int userID, String label, int count){
+			this.userID = userID;
 			this.label=label;
 			this.count=count;
+		}
+		public int getUserID(){
+			return userID;
 		}
 		public String getLabel(){
 			return label;
@@ -79,7 +94,7 @@ public class Storage {
 		public boolean equals(Object obj){
 			if(obj instanceof KeyStorage){
 				KeyStorage k = (KeyStorage) obj;
-				return this.getLabel().equals(k.getLabel()) && this.getCount()==k.getCount();
+				return this.userID==k.getUserID() && this.getLabel().equals(k.getLabel()) && this.getCount()==k.getCount();
 			}
 			return false;
 		}
