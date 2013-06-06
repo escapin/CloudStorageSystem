8d976d8cb7482b231cd6e7d7cb9522b99d834447
Tomasz Truderung <ttruderung@gmail.com>
2013-06-06 09:38:07 +0200

    a small change in the ClientTest

--- src/de/uni/trier/infsec/tests/ClientTest.java
+++ src/de/uni/trier/infsec/tests/ClientTest.java
@@ -1,18 +1,14 @@
 package de.uni.trier.infsec.tests;
 
-import static org.junit.Assert.*;
-
+import junit.framework.TestCase;
 import org.junit.Test;
-
 import de.uni.trier.infsec.cloudStorage.NetworkInterface;
-import de.uni.trier.infsec.cloudStorage.Params;
 import de.uni.trier.infsec.cloudStorage.Server;
 import de.uni.trier.infsec.cloudStorage.Server.MalformedMessage;
 import de.uni.trier.infsec.functionalities.pki.real.PKIError;
-import de.uni.trier.infsec.lib.network.NetworkClient;
 import de.uni.trier.infsec.lib.network.NetworkError;
 
-public class ClientTest {
+public class ClientTest extends TestCase {
 
 	@Test
 	public void test() {
