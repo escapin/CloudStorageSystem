6870b7ac9f5c4fbe526ce3b2fcfcc26adf4c27a2
Tomasz Truderung <ttruderung@gmail.com>
2013-06-27 11:26:19 +0200

    small changes in class Params

--- src/de/uni/trier/infsec/cloudStorage/Params.java
+++ src/de/uni/trier/infsec/cloudStorage/Params.java
@@ -6,16 +6,7 @@ public class Params {
 
 	public static byte[] PKI_DSIG_DOMAIN = "PKI_DSIG".getBytes();
 	public static byte[] PKI_ENC_DOMAIN  = "PKI_ENC".getBytes();
-	
-	public static final String PATH_STORAGE = System.getProperty("java.io.tmpdir") + File.separator + "CloudStorage" + File.separator;
-	public static final String STORAGE_DB = PATH_STORAGE + File.separator + "cloud_storage.db";
-	public static final String PATH_SERVER = Params.PATH_STORAGE + File.separator + "server.info";
-	public static final String PATH_USER = Params.PATH_STORAGE + File.separator;
-
-	
-	
 	public static int SERVER_ID = 1;
-	// public static String SERVER_NAME = "192.168.1.1";
 	public static String SERVER_NAME = "localhost";
 	public static int SERVER_PORT= 7075;
 
@@ -30,5 +21,11 @@ public class Params {
 	public static byte[] RETRIEVE_OK="RETRIEVE_OK".getBytes();
 	public static byte[] RETRIEVE_FAIL="RETRIEVE_FAIL".getBytes();
 	public static byte[] LAST_COUNTER="LAST_COUNTER".getBytes();
+
+	// Storage
+	public static final String PATH_STORAGE = System.getProperty("java.io.tmpdir") + File.separator + "CloudStorage" + File.separator;
+	public static final String STORAGE_DB = PATH_STORAGE + File.separator + "cloud_storage.db";
+	public static final String PATH_SERVER = Params.PATH_STORAGE + File.separator + "server.info";
+	public static final String PATH_USER = Params.PATH_STORAGE + File.separator;
 }
 
